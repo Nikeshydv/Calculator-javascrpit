01babecc57d497ba93fce9a96f1eced6e06bcb4b
Nikesh Yadav <105097035+Nikeshydv@users.noreply.github.com>
2024-07-13 23:57:00 +0530

    calculator

---
+++ scrpit.js
@@ -0,0 +1,16 @@
+let input=document.getElementById("");
+let button=document.querySelectorAll("button");
+
+
+let string ="";
+
+let arr=Array.from(button);
+arr.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        if(e.target.innerHtml=='='){
+            string
+        }
+    }
+
+
+})
