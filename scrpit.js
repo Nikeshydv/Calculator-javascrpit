05126e6c11486f3751f09b1fc339c4b89166489e
Nikesh Yadav <105097035+Nikeshydv@users.noreply.github.com>
2024-07-14 00:03:03 +0530

    second commit

--- scrpit.js
+++ scrpit.js
@@ -8,9 +8,23 @@ let arr=Array.from(button);
 arr.forEach((button)=>{
     button.addEventListener("click",(e)=>{
         if(e.target.innerHtml=='='){
-            string
+            string=eval(string);
+            input.value=string;
         }
-    }
 
+        else if(e.target.innerHtml=='Del'){
+            string=string.substring(0,string.length-1);
+            input.value=string;
+        }
 
+        else if(e.target.innerHtml=='Ac'){
+            string="";
+            input.value=string;
+        }
+
+        else
+        string+=e.target.innerHtml
+    input.value=string;
+    })
 })
+
